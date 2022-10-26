ea5afce5bed2361ffac586a91bbc7af181cac731
Niclas <115414592+IconNT06@users.noreply.github.com>
2022-10-26 09:31:23 +0100

    Update Guess the Number.py

--- Guess the Number.py
+++ Guess the Number.py
@@ -6,11 +6,9 @@ async def nummer(ctx, *, number=0):
   if number == correct_number:
     embed = discord.Embed(title=f'Deine Nummer: {number}', color=	0x2ecc71)
     embed.add_field(name='Dies war die richtige Nummer!', value="Danke fürs Spielen!")
-    embed.set_footer(text='Developed by IconNT06', icon_url='https://media.discordapp.net/attachments/783038584176640020/964994528413114429/standard_8_1.png')
     await ctx.send(embed=embed)
   
   else:
     embed = discord.Embed(title=f'Deine Nummer: {number}', color=0xFF0000)
     embed.add_field(name="Dies war die falsche Nummer", value="Danke fürs Spielen!")
-    embed.set_footer(text='Developed by IconNT06', icon_url='https://media.discordapp.net/attachments/783038584176640020/964994528413114429/standard_8_1.png')
     await ctx.send(embed=embed)
